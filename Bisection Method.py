0bce853230aafb15a1deab7a0cab55407cca6b55
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-10-28 02:36:41 +0600

    Update Bisection Method.py

--- Bisection Method.py
+++ Bisection Method.py
@@ -16,42 +16,48 @@ def bisection(func_str,a,b,tol):
       """
 
     def f(x):
-        return eval(func_str)  # eval() is a built-in Python function that takes a string and executes it as Python code
+        return eval(func_str)
 
     if f(a)*f(b)>=0:
         print("Error: f(a) and f(b) must have opposite signs.")
         return None
 
     while abs(b-a) > tol:
-        mid = (a+b)/2 #midpooint
 
-        if f(mid) == 0:
-            return mid #exact Root
-        if f(a)*f(mid) > 0:
-            a=mid       # if f(mid) and f(a) are both same sign , replace a with mid
+        c = (a+b)/2 #midpooint
+
+        if f(c) == 0:
+            return c #exact Root
+        if f(a)*f(c) > 0:
+            a=c       # if f(c) and f(a) are both same sign , replace a with c
         else:
-            b=mid       # else f(mid) and f(b) are both same sign , replace b with mid
+            b=c       # else f(c) and f(b) are both same sign , replace b with c
 
     return (a+b)/2      # Midpoint is the best approximation
 
 func= input("Enter any equation using variable 'x' only e.g. ( x**3-3*x+1 ) : ")
-a= float(input("Enter lower bound: "))
-b=float(input("Enter upper bound: "))
+a = float(input("Enter lower limit a: "))
+b = float(input("Enter upper limit b: "))
 tolerance=float(input("Enter Tolerance (desired_accuracy) e.g (0.00001): "))
 
 root = bisection(func,a,b,tolerance)
 
-print(f"Root = {root:.5f}")
+print(f"Root = {root:.5f} [Approx...]")
 
 #plotting
 x= np.linspace(-5,5,100)
 y= eval(func)
 
 plt.plot(x,y)
+
 plt.axvline(0,color ='black')
 plt.axhline(0,color = 'black')
 plt.scatter(root,0,color= 'red') # mark the root
+
 plt.xlabel('x')
 plt.ylabel('f(x)')
+plt.grid(True)
 plt.title(f"Bisection Method:{func}")
+
 plt.show()
+
