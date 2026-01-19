5ce026ab60bcdf495fa9ab49e6542d8ba14f7eb5
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2026-01-19 23:20:22 +0600

    Add files via upload

---
+++ Backward diff interpolation.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [0, 1, 2, 3]
+y = [1, 3, 7, 13]
+
+h = x[1] - x[0]
+n = len(y)
+
+diff = [y.copy()]
+
+for i in range(1, n):
+    temp = []
+    for j in range(n - i):
+        temp.append(diff[i-1][j+1] - diff[i-1][j])
+    diff.append(temp)
+
+def newton_backward(x_val):
+    p = (x_val - x[-1]) / h
+    result = y[-1]
+    fact = 1
+    p_term = 1
+
+    for i in range(1, n):
+        p_term *= (p + (i - 1))
+        fact *= i
+        result += (p_term * diff[i][-1]) / fact
+
+    return result
+
+x_val = 2.7
+print("Interpolated value at x =", x_val, "is", newton_backward(x_val))
+
+# Plot
+x_plot = np.linspace(min(x), max(x), 100)
+y_plot = [newton_backward(xi) for xi in x_plot]
+
+plt.plot(x_plot, y_plot, label="Newton Backward")
+plt.scatter(x, y, color='red', label="Data Points")
+plt.legend()
+plt.grid(True)
+plt.show()
